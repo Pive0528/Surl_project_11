86202a73068f045aa7577c721bca3bd9064792b8
Pive0528 <pive0528@gmail.com>
2024-11-11 10:51:22 +0900

    Surl 엔티티화

---
+++ src/main/java/com/koreait/surl_project_11/domain/surl/surl/repository/SurlRepository.java
@@ -0,0 +1,5 @@
+package com.koreait.surl_project_11.domain.surl.surl.repository;
+import com.koreait.surl_project_11.domain.surl.surl.entity.Surl;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface SurlRepository extends JpaRepository<Surl, Long> {
+}
