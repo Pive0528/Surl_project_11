4d5331629cd66632b77d2058fa67aaec5c7dc211
Pive0528 <pive0528@gmail.com>
2024-11-28 14:56:51 +0900

    JWT를 사용하여 인증

--- src/main/java/com/koreait/surl_project_11/domain/auth/auth/service/AuthTokenService.java
+++ src/main/java/com/koreait/surl_project_11/domain/auth/auth/service/AuthTokenService.java
@@ -1,4 +1,5 @@
 package com.koreait.surl_project_11.domain.auth.auth.service;
+
 import com.koreait.surl_project_11.domain.member.member.entity.Member;
 import com.koreait.surl_project_11.global.app.AppConfig;
 import io.jsonwebtoken.Claims;
@@ -6,7 +7,10 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.util.Date;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class AuthTokenService {
@@ -15,13 +19,36 @@ public class AuthTokenService {
                 .add("id", member.getId())
                 .add("username", member.getUsername())
                 .build();
+
         Date issuedAt = new Date();
         Date expiration = new Date(issuedAt.getTime() + expireSeconds * 1000);
+
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(issuedAt)
                 .setExpiration(expiration)
                 .signWith(SignatureAlgorithm.HS256, AppConfig.getJwtSecretKey())
                 .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(AppConfig.getJwtSecretKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Map<String, Object> getDataFrom(String token) {
+        Claims payload = Jwts.parser()
+                .setSigningKey(AppConfig.getJwtSecretKey())
+                .build()
+                .parseClaimsJws(token).getPayload();
+
+        return Map.of(
+                "id", payload.get("id", Integer.class),
+                "username", payload.get("username", String.class)
+        );
     }
 }
